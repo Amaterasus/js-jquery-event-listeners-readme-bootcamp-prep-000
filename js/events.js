3935774de586f04b70093bdbe272e79b13df8447
Skye Geraghty <skyetay03@googlemail.com>
2019-12-17 11:27:39 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt()
 {
   $("img").on("load", function()
   {
-    $('#img').append('<img id="theImg" src="https://s3.amazonaws.com/learn-verified/painting-with-dog.gif" />')
+    $('#img').append('<img id="tasty"')
   });
 }
 
